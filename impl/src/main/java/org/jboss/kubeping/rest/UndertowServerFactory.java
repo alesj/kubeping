27f0a651c47290c3406b86965cbed649cfcdf147
David Ward <dward@redhat.com>
2015-03-18 10:05:24 -0400

    add EAP support

--- impl/src/main/java/org/jboss/kubeping/rest/UndertowServerFactory.java
+++ impl/src/main/java/org/jboss/kubeping/rest/UndertowServerFactory.java
@@ -16,16 +16,21 @@
 
 package org.jboss.kubeping.rest;
 
+import java.util.logging.Logger;
+
 import org.jgroups.Channel;
 
 /**
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class UndertowServerFactory implements ServerFactory {
+    private static final Logger log = Logger.getLogger(UndertowServerFactory.class.getName());
+
     public boolean isAvailable() {
         try {
             return UndertowServerFactory.class.getClassLoader().loadClass("io.undertow.Undertow") != null;
         } catch (Exception e) {
+            log.warning(e.getClass().getName() + ": " + e.getMessage());
             return false;
         }
     }
